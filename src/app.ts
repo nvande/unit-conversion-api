50c0e8c8513e203453eb6d1228dcb2bb35ccf3ff
nick <nick.vanderwoude@donefirst.com>
2024-04-28 00:43:34 -0500

    feat: github actions

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
-import express from "express";
+import express, { Request, Response } from 'express';
 
 const app = express();
-const port = process.env.PORT || 3000;
-app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req: Request, res: Response): void => {
+    res.status(200).send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+if (require.main === module) {
+    const port: number = parseInt(process.env.PORT as string, 10) || 3000;
+    app.listen(port, (): void => {
+        console.log(`Server is running on port ${port}`);
+    });
+}
+
+export default app;
