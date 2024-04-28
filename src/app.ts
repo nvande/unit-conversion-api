38b7da37949e7dc7721e4ebc058f8321041f72e7
nick <nick.vanderwoude@donefirst.com>
2024-04-28 03:40:24 -0500

    feat: EB support

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,10 @@ app.get('/', (req: Request, res: Response): void => {
     res.status(200).send('Hello World!');
 });
 
-if (require.main === module) {
-    const port: number = parseInt(process.env.PORT as string, 10) || 3000;
-    app.listen(port, (): void => {
-        console.log(`Server is running on port ${port}`);
-    });
-}
+const port = process.env.PORT || 3000;
+
+app.listen(port, (): void => {
+    console.log(`Server is running on port ${port}`);
+});
 
 export default app;
